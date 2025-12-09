f8a38814d7de722c1c3425a6cba6c5ac1b03d76a
shaiksharuk-netizen <shaik.sharuk@forsysinc.com>
2025-12-09 23:08:57 +0530

    Update back button functionality
    Change back button to redirect to base frontend URL.

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2145,7 +2145,9 @@ const HomePage = () => {
       <div className="header-buttons-container">
         <button 
           className="header-back-btn"
-          onClick={() => navigate(-1)}
+          onClick={() => {
+            window.location.href = `${process.env.REACT_APP_BASE_FRONTEND_URL}`;
+          }} 
           title="Go Back"
         >
           <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
